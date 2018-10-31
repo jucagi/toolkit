8d71d0993241ca9ae345a0f6802495b9bcff8fe4
Juan Carlos Giraldo Cardozo <jgiraldocardozo@gmail.com>
2018-10-30 22:20:45 -0500

    Versión inicial de demostración, barra de navegación.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container fluid>
+    <div>
+      <v-alert
+        :value="true"
+        type="success"
+      >
+        This is a success alert.
+      </v-alert>
+
+      <v-alert
+        :value="true"
+        type="info"
+      >
+        This is a info alert.
+      </v-alert>
+
+      <v-alert
+        :value="true"
+        type="warning"
+      >
+        This is a warning alert.
+      </v-alert>
+
+      <v-alert
+        :value="true"
+        type="error"
+      >
+        This is a error alert.
+      </v-alert>
+    </div>
+    <v-layout row>
+      <v-flex>
+        <v-img :src="logoToolkit"></v-img>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import logoToolkit from '@/assets/logoToolkit.svg'
+
+export default {
+  data () {
+    return {
+      logoToolkit: logoToolkit
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#C51162',
+    secondary: '#FFEA00',
+    accent: '#009999',
+    error: '#FF6600',
+    info: '#8BC34A',
+    success: '#FFA726',
+    warning: '#7E57C2'
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
